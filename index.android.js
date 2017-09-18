b9e8bb2d6618295c0e58d22d2bbbaa291f3ac364
Alexey <alevor657@gmail.com>
2017-09-18 09:14:04 +0200

    Initial commit

--- index.android.js
+++ index.android.js
@@ -2,4 +2,5 @@
 import { AppRegistry } from 'react-native';
 import setup from './js/setup';
 
-AppRegistry.registerComponent('NativeStarterKit', setup);
+// AppRegistry.registerComponent('NativeStarterKit', setup);
+AppRegistry.registerComponent('Videosupport', setup);
